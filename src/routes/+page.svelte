1925006299d68abb4679cd677512d0e49ec299ca
Jimm <jimren@stagwell.tech>
2023-11-20 16:52:58 -0500

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script>
     import {browser} from "$app/environment";
     import {goto} from "$app/navigation";
-	  goto("/home");
+	$:if(browser) {
+
+        goto("/home");
+    }
 </script>
 
 <svelte:head>
