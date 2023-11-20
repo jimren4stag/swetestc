021de26f82dd5d542184cf487940c155b60a1260
Jimm <jimren@stagwell.tech>
2023-11-20 16:42:38 -0500

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,3 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
 
 <svelte:head>
 	<title>Home</title>
@@ -11,12 +6,6 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
 		to your new<br />SvelteKit app
 	</h1>
@@ -25,7 +14,6 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
