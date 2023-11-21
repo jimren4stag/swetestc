610447b4fb9a741c89083d0adc90d9ae44692a62
Jimm <jimren@stagwell.tech>
2023-11-20 20:50:28 -0500

    test

--- src/routes/tests/[test_name]/+page.svelte
+++ src/routes/tests/[test_name]/+page.svelte
@@ -2,18 +2,20 @@
     export let data;
     import {onMount} from "svelte";
     import ResultsPage from "$lib/sleepapp/shared/results_page.svelte";
-    let Test;
+    let arithmetic_ability;
+    let simple_attention;
+    let episodic_memory;
+    let working_memory;
     let stats;
     
     onMount(async() => {
-        console.log("jimstats");
-        Test = (await import("../../../lib/sleepapp/tests/working_memory.svelte")).default;
-        console.log("jimstats0");
+        arithmetic_ability = (await import("../../../lib/sleepapp/tests/arithmetic_ability.svelte")).default;
+        simple_attention = (await import("../../../lib/sleepapp/tests/simple_attention.svelte")).default;
+        episodic_memory = (await import("../../../lib/sleepapp/tests/episodic_memory.svelte")).default;
+        working_memory = (await import("../../../lib/sleepapp/tests/working_memory.svelte")).default;
         await fetch("/cookies?"+data.params.test_name, {method:"GET"})
         .then(response => response.json())
         .then(d => stats = d.body);
-        console.log(stats);
-        console.log("jimstats2");
     });
 
     const handleComplete = (e) => {
@@ -38,8 +40,14 @@
     }
 
 </script>
-{#if !data.completed_tests.includes(data.params.test_name)}
-<svelte:component this={Test} on:complete={handleComplete}/>
+{#if !data.completed_tests.includes("arithmetic_ability") && data.params.test_name == "arithmetic_ability"}
+<svelte:component this={arithmetic_ability} on:complete={handleComplete}/>
+{:else if !data.completed_tests.includes("simple_attention") && data.params.test_name == "simple_attention"}
+<svelte:component this={simple_attention} on:complete={handleComplete}/>
+{:else if !data.completed_tests.includes("episodic_memory") && data.params.test_name == "episodic_memory"}
+<svelte:component this={episodic_memory} on:complete={handleComplete}/>
+{:else if !data.completed_tests.includes("working_memory") && data.params.test_name == "working_memory"}
+<svelte:component this={working_memory} on:complete={handleComplete}/>
 {:else}
 <div>
     <h2>{data.params.test_name} test results</h2>
